a4b4c37be6e1ec5d75a3d72f54aa3406e880647c
Kaine Bruce <kaineyb@gmail.com>
2024-04-14 19:46:23 +0100

    added transition

---
+++ src/nodes/transition.js
@@ -0,0 +1,59 @@
+//  TODO ZTP was rushed, needs checking
+//  Also these are ZONAL not Cities.
+
+const ZTP_LABEL = "Zone Transition Point";
+
+const zone_transition_points = [
+  //  DC
+  ["Glenumbra", "Stormhaven", ZTP_LABEL],
+  ["Stormhaven", "Glenumbra", ZTP_LABEL],
+
+  ["Rivenspire", "Stormhaven", ZTP_LABEL],
+  ["Stormhaven", "Rivenspire", ZTP_LABEL],
+
+  ["Stormhaven", "Wrothgar", ZTP_LABEL],
+  ["Wrothgar", "Stormhaven", ZTP_LABEL],
+
+  ["Stormhaven", "Bangkorai", ZTP_LABEL],
+  ["Bangkorai", "Stormhaven", ZTP_LABEL],
+
+  ["Bangkorai", "Wrothgar", ZTP_LABEL],
+  ["Wrothgar", "Bangkorai", ZTP_LABEL],
+
+  ["Craglorn", "Bangkorai", ZTP_LABEL],
+  ["Bangkorai", "Craglorn", ZTP_LABEL],
+
+  //  AD
+
+  ["Grahtwood", "Reaper's March", ZTP_LABEL],
+  ["Reaper's March", "Grahtwood", ZTP_LABEL],
+
+  ["Malabal Tor", "Reaper's March", ZTP_LABEL],
+  ["Reaper's March", "Malabal Tor", ZTP_LABEL],
+
+  ["Malabal Tor", "Grahtwood", ZTP_LABEL],
+  ["Grahtwood", "Malabal Tor", ZTP_LABEL],
+
+  ["Malabal Tor", "Greenshade", ZTP_LABEL],
+  ["Greenshade", "Malabal Tor", ZTP_LABEL],
+
+  ["Greenshade", "Grahtwood", ZTP_LABEL],
+  ["Grahtwood", "Greenshade", ZTP_LABEL],
+
+  //  EP
+
+  ["East March", "The Rift", ZTP_LABEL],
+  ["The Rift", "East March", ZTP_LABEL],
+
+  ["The Rift", "Stonefalls", ZTP_LABEL],
+  ["Stonefalls", "The Rift", ZTP_LABEL],
+
+  ["Stonefalls", "Bal Foyen", ZTP_LABEL],
+  ["Bal Foyen", "Stonefalls", ZTP_LABEL],
+
+  ["Stonefalls", "Deshaan", ZTP_LABEL],
+  ["Deshaan", "Stonefalls", ZTP_LABEL],
+
+  ["Deshaan", "Shadowfen", ZTP_LABEL],
+  ["Shadowfen", "Deshaan", ZTP_LABEL],
+];
