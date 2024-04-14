e6d313ede52d81035566524605e381cde6a0fe0f
Kaine Bruce <kaineyb@gmail.com>
2024-04-14 19:42:02 +0100

    added boatswain

---
+++ src/nodes/boatswain.js
@@ -0,0 +1,36 @@
+export const factionBoatswain = [
+  //  From Data on https://en.uesp.net/wiki/Online:Boatswains
+
+  //  Aldmeri Dominion
+  //  Sugar-Claws will take you both ways
+  ["Vulkhel Guard", "Eagle's Strand", "Sugar-Claws"],
+  ["Eagle's Strand", "Vulkhel Guard", "Sugar-Claws"],
+  //  Scout Linimondil will take you both ways
+  ["Skywatch", "Haven", "Scout Linimondil"],
+  ["Haven", "Skywatch", "Scout Linimondil"],
+  //  Daggerfall Covenant
+  //  Gilzir will take you three ways
+  ["Daggerfall", "Port Hunding", "Gilzir"],
+  ["Daggerfall", "Stonetooth Fortress", "Gilzir"],
+  ["Port Hunding", "Daggerfall", "Gilzir"],
+  ["Port Hunding", "Stonetooth Fortress", "Gilzir"],
+  ["Stonetooth Fortress", "Daggerfall", "Gilzir"],
+  ["Stonetooth Fortress", "Port Hunding", "Gilzir"],
+  ["Wayrest", "Sentinel", "Zihlijdel"],
+  ["Sentinel", "Wayrest", "Captain Carlier"],
+  //  Ebonheart Pact
+  //  TODO The Boatmaster is quest NPC that requires an invite?
+  //  ["Mournhold ", "Shad Astula", "The Boatmaster"],
+
+  //  Dastas Arenim is not here if you've evactuated Bleakrock Village!
+  //  ["Bleakrock Village", "Seyda Neen", "Dastas Arenim"],
+
+  ["Bleakrock Village", "Davon's Watch", "Liezl"],
+  ["Bleakrock Village", "Dhalmora", "Liezl"],
+  ["Davon's Watch", "Bleakrock Village", "Liezl"],
+  ["Davon's Watch", "Dhalmora", "Liezl"],
+  ["Dhalmora", "Bleakrock Village", "Liezl"],
+  ["Dhalmora", "Davon's Watch", "Liezl"],
+  ["Alten Corimont", "Windhelm", "Muz-Muz"],
+  ["Windhelm", "Alten Corimont", "Muz-Muz"],
+];
