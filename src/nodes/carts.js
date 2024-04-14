7be214b463fbd721d6d02e78eaaae32ff1539e42
Kaine Bruce <kaineyb@gmail.com>
2024-04-14 19:44:02 +0100

    added carts

---
+++ src/nodes/carts.js
@@ -0,0 +1,24 @@
+//  TODO Carts was rushed, needs checking
+
+const CART_LABEL = "Self-driven Cart";
+
+export const carts = [
+  //  Wrothgar
+  ["Orsinium", "Vulkhel Guard", CART_LABEL],
+  ["Orsinium", "Daggerfall", CART_LABEL],
+  ["Orsinium", "Davon's Watch", CART_LABEL],
+
+  ["Vulkhel Guard", "Orsinium", CART_LABEL],
+  ["Wayrest", "Orsinium", CART_LABEL],
+  ["Daggerfall", "Orsinium", CART_LABEL],
+
+  //  Craglorn
+
+  ["Belkarth", "Mournhold", CART_LABEL],
+  ["Belkarth", "Wayrest", CART_LABEL],
+  ["Belkarth", "Elden Root", CART_LABEL],
+
+  ["Mournhold", "Belkarth", CART_LABEL],
+  ["Wayrest", "Belkarth", CART_LABEL],
+  ["Elden Root", "Belkarth", CART_LABEL],
+];
