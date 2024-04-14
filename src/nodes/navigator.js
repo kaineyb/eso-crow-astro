fa564dde00f49a765041e31ab5172edc275d9f42
Kaine Bruce <kaineyb@gmail.com>
2024-04-14 19:45:19 +0100

    added navigators

---
+++ src/nodes/navigator.js
@@ -0,0 +1,142 @@
+export const navigator = [
+  //  Alliance Navigators
+
+  //  AD - Selandilwen
+  ["Vulkhel Guard", "Elden Root", "Selandilwen"],
+  ["Vulkhel Guard", "Rawl'kha", "Selandilwen"],
+  ["Vulkhel Guard", "Daggerfall", "Selandilwen"],
+  ["Vulkhel Guard", "Davon's Watch", "Selandilwen"],
+
+  ["Elden Root", "Woodhearth", "Selandilwen"],
+  ["Elden Root", "Vulkhel Guard", "Selandilwen"],
+  ["Elden Root", "Wayrest", "Selandilwen"],
+  ["Elden Root", "Mournhold", "Selandilwen"],
+
+  ["Woodhearth", "Vulkwasten", "Selandilwen"],
+  ["Woodhearth", "Elden Root", "Selandilwen"],
+  ["Woodhearth", "Shornhelm", "Selandilwen"],
+  ["Woodhearth", "Stormhold", "Selandilwen"],
+
+  ["Vulkwasten", "Rawl'kha", "Selandilwen"],
+  ["Vulkwasten", "Woodhearth", "Selandilwen"],
+  ["Vulkwasten", "Sentinel", "Selandilwen"],
+  ["Vulkwasten", "Windhelm", "Selandilwen"],
+
+  ["Rawl'kha", "Vulkhel Guard", "Selandilwen"],
+  ["Rawl'kha", "Vulkwasten", "Selandilwen"],
+  ["Rawl'kha", "Evermore", "Selandilwen"],
+  ["Rawl'kha", "Riften", "Selandilwen"],
+
+  //  EP - Falvis Raram
+
+  ["Davon's Watch", "Mournhold", "Falvis Raram"],
+  ["Davon's Watch", "Riften", "Falvis Raram"],
+  ["Davon's Watch", "Vulkhel Guard", "Falvis Raram"],
+  ["Davon's Watch", "Daggerfall", "Falvis Raram"],
+
+  ["Mournhold", "Stormhold", "Falvis Raram"],
+  ["Mournhold", "Davon's Watch", "Falvis Raram"],
+  ["Mournhold", "Elden Root", "Falvis Raram"],
+  ["Mournhold", "Wayrest", "Falvis Raram"],
+
+  ["Stormhold", "Windhelm", "Falvis Raram"],
+  ["Stormhold", "Mournhold", "Falvis Raram"],
+  ["Stormhold", "Woodhearth", "Falvis Raram"],
+  ["Stormhold", "Shornhelm", "Falvis Raram"],
+
+  ["Windhelm", "Riften", "Falvis Raram"],
+  ["Windhelm", "Stormhold", "Falvis Raram"],
+  ["Windhelm", "Vulkwasten", "Falvis Raram"],
+  ["Windhelm", "Sentinel", "Falvis Raram"],
+
+  ["Riften", "Davon's Watch", "Falvis Raram"],
+  ["Riften", "Windhelm", "Falvis Raram"],
+  ["Riften", "Rawl'kha", "Falvis Raram"],
+  ["Riften", "Evermore", "Falvis Raram"],
+
+  //  DC - Azoufah
+
+  ["Daggerfall", "Wayrest", "Azoufah"],
+  ["Daggerfall", "Evermore", "Azoufah"],
+  ["Daggerfall", "Vulkhel Guard", "Azoufah"],
+  ["Daggerfall", "Davon's Watch", "Azoufah"],
+
+  ["Wayrest", "Shornhelm", "Azoufah"],
+  ["Wayrest", "Daggerfall", "Azoufah"],
+  ["Wayrest", "Elden Root", "Azoufah"],
+  ["Wayrest", "Mournhold", "Azoufah"],
+
+  ["Shornhelm", "Sentinel", "Azoufah"],
+  ["Shornhelm", "Wayrest", "Azoufah"],
+  ["Shornhelm", "Woodhearth", "Azoufah"],
+  ["Shornhelm", "Stormhold", "Azoufah"],
+
+  ["Sentinel", "Evermore", "Azoufah"],
+  ["Sentinel", "Shornhelm", "Azoufah"],
+  ["Sentinel", "Vulkwasten", "Azoufah"],
+  ["Sentinel", "Windhelm", "Azoufah"],
+
+  ["Evermore", "Daggerfall", "Azoufah"],
+  ["Evermore", "Sentinel", "Azoufah"],
+  ["Evermore", "Rawl'kha", "Azoufah"],
+  ["Evermore", "Riften", "Azoufah"],
+
+  //  TODO Morrowind - Check Morrowind
+  //  Captain Jenassa
+  ["Elden Root", "Seyda Neen", "Captain Jenassa"],
+  ["Elden Root", "Vivec", "Captain Jenassa"],
+
+  ["Daggerfall", "Seyda Neen", "Captain Jenassa"],
+  ["Daggerfall", "Vivec", "Captain Jenassa"],
+
+  ["Davon's Watch", "Seyda Neen", "Captain Jenassa"],
+  ["Davon's Watch", "Vivec", "Captain Jenassa"],
+
+  ["Stormhold", "Seyda Neen", "Captain Jenassa"],
+  ["Stormhold", "Vivec", "Captain Jenassa"],
+
+  //  Bolnora Romavel
+  ["Tel Mora", "Sadrith Mora", "Bolnora Romavel"],
+
+  //  Ranor Sadralo
+  ["Gnisis", "Sadrith Mora", "Bolnora Romavel"],
+
+  //  Rinori Mathendis
+  ["Sadrith Mora", "Gnisis", "Rinori Mathendis"],
+  ["Sadrith Mora", "Tel Mora", "Rinori Mathendis"],
+  ["Sadrith Mora", "Vivec", "Rinori Mathendis"],
+
+  //  Synda Imyam
+  ["Vivec", "Sadrith Mora", "Synda Imyam"],
+  ["Vivec", "Elden Root", "Synda Imyam"],
+  ["Vivec", "Mournhold", "Synda Imyam"],
+  ["Vivec", "Wayrest", "Synda Imyam"],
+
+  //  Taros Evosa
+  ["Seyda Neen", "Bleakrock Village", "Taros Evosa"],
+  ["Seyda Neen", "Port Hunding", "Taros Evosa"],
+  ["Seyda Neen", "Eagle's Strand", "Taros Evosa"],
+
+  //  Davak Hlaren
+  ["Port Hunding", "Seyda Neen", "Davak Hlaren"],
+
+  //  Sathasa Falen
+  ["Eagle's Strand", "Seyda Neen", "Sathasa Falen"],
+
+  //  TODO - CHECK Summerset -
+  //  Ciryelda - also found in Port Hunding?
+
+  //  Ciryelda is not here if you've evactuated Bleakrock Village!
+  ["Bleakrock Village", "Alinor", "Ciryelda"],
+
+  ["Davon's Watch", "Alinor", "Ciryelda"],
+  ["Eagle's Strand", "Alinor", "Ciryelda"],
+
+  ["Elden Root", "Alinor", "Ciryelda"],
+  ["Mournhold", "Alinor", "Ciryelda"],
+  ["Wayrest", "Alinor", "Ciryelda"],
+
+  ["Alinor", "Elden Root", "Ciryelda"],
+  ["Alinor", "Mournhold", "Ciryelda"],
+  ["Alinor", "Wayrest", "Ciryelda"],
+];
