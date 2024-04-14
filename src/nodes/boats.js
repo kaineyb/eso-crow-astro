d7df1aee41e6290063de2913cf6dbc64ce883b87
Kaine Bruce <kaineyb@gmail.com>
2024-04-14 19:40:29 +0100

    added boats

---
+++ src/nodes/boats.js
@@ -0,0 +1,39 @@
+export const boats = [
+  ["Abah's Landing", "Vivec", "Unnamed Sailboat"], // One way
+  ["Abah's Landing", "Windhelm", "Harbor Skiff"],
+  ["Abah's Landing", "Woodhearth", "Harbor Skiff"],
+  ["Abah's Landing", "Wayrest", "Harbor Skiff"],
+  // Anvil
+  ["Anvil", "Vivec", "The Razorbill"], // One way
+  ["Anvil", "Vulkhel Guard", "The Egret"],
+  ["Anvil", "Davon's Watch", "The Osprey"],
+  ["Anvil", "Daggerfall", "The Heron"],
+  ["Anvil", "Shimmerene", "The Golden Gryphon"],
+  // Daggerfall
+  ["Daggerfall", "Anvil", "Harbor Skiff"],
+  ["Daggerfall", "Davon's Watch", "The Hotspur"],
+  ["Daggerfall", "Vulkhel Guard", "The Lydia"],
+  //  Murkmire Boat
+  ["Daggerfall", "Lilmoth", "Southern Dock - Murkmire Boat"],
+  // Davon's Watch
+  ["Davon's Watch", "Anvil", "Harbor Skiff"],
+  ["Davon's Watch", "Vulkhel Guard", "The Rusty Argonian Blade"],
+  ["Davon's Watch", "Daggerfall", "Summerwind"],
+  //  Mournhold
+  ["Mournhold", "Vivec", "Unnamed Sailboat"],
+  // Vivec
+  ["Vivec", "Mournhold", "Coastal Skiff"],
+  // Vulkhel Guard
+  ["Vulkhel Guard", "Anvil", "Harbor Skiff"],
+  ["Vulkhel Guard", "Davon's Watch", "Harbor Skiff"],
+  ["Vulkhel Guard", "Daggerfall", "The Interim Suitor"],
+  ["Vulkhel Guard", "Lilmoth", "Murkmire Boat"],
+  // Wayrest
+  ["Wayrest", "Vivec", "Unnamed Sailboat"], // One way
+  ["Wayrest", "Abah's Landing", "Harbor Skiff"],
+  // Windhelm
+  ["Windhelm", "Abah's Landing", "Longboat"],
+  // Woodhearth
+  ["Woodhearth", "Vivec", "Unnamed Sailboat"], // One way
+  ["Woodhearth", "Abah's Landing", "Harbor Skiff"],
+];
