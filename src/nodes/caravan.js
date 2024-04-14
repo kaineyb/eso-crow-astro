5117045170d4c0598c96570db092bb02ff4c0f1f
Kaine Bruce <kaineyb@gmail.com>
2024-04-14 19:44:15 +0100

    added caravan

---
+++ src/nodes/caravan.js
@@ -0,0 +1,8 @@
+const baandariCaravanStop = [
+  ["Rimmen", "Elden Root", "Zemmarga"],
+  ["Elden Root", "Rimmen", "Zemmarga"],
+  ["Rimmen", "Wayrest", "Zemmarga"],
+  ["Wayrest", "Rimmen", "Zemmarga"],
+  ["Rimmen", "Mournhold", "Zemmarga"],
+  ["Mournhold", "Rimmen", "Zemmarga"],
+];
