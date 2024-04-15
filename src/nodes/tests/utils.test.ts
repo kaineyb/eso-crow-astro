11e14fd93ba6279c169562bff34c941a572a7bfd
Kaine Bruce <kaineyb@gmail.com>
2024-04-15 20:14:28 +0100

    added tests

--- src/nodes/tests/utils.test.ts
+++ src/nodes/tests/utils.test.ts
@@ -1,20 +1,51 @@
 // sum.test.js
 import { expect, test } from "vitest";
 
-import { Edge } from "../types";
-import { getStartsAndEnds } from "../utils";
+import { Edge, type EdgeType } from "../types";
+import { getBiDirectional, getStartsAndEnds } from "../utils";
+import { directional } from "../all";
 
-test("", () => {
+test("Test that getBiDirectional is doesn't return directional Edges", () => {
+  // Arrange
+  const directionalEdge = new Edge("Three", "One", "", "");
+
   const testEdges: Edge[] = [
-    new Edge("Start1", "End1", "Via1", "ET1"),
-    new Edge("Start2", "End2", "Via2", "ET2"),
-    new Edge("Start3", "End3", "Via3", "ET3"),
-    new Edge("Start4", "End4", "Via4", "ET4"),
-    new Edge("Start5", "End5", "Via5", "ET5"),
+    new Edge("One", "Two", "", ""),
+    new Edge("Two", "One", "", ""),
+    new Edge("One", "Four", "", ""),
+    new Edge("Four", "One", "", ""),
+    directionalEdge,
   ];
 
   const [start, end] = getStartsAndEnds(testEdges);
 
-  console.log("start", start);
-  console.log("end", end);
+  // Act
+  const bidirectional = getBiDirectional(testEdges, start, end);
+
+  // assert
+  expect(bidirectional.includes(directionalEdge)).toBe(false);
+});
+
+test("Test that bidirectional Edges are equivalent", () => {
+  const baseScenario: Edge[] = [
+    new Edge("One", "Two", "", "", true),
+    new Edge("Two", "One", "", "", true),
+    new Edge("Three", "Four", "", ""),
+  ];
+
+  const testScenario: Edge[] = [
+    new Edge("One", "Two", "", ""),
+    new Edge("Three", "Four", "", ""),
+  ];
+
+  function createEdges(source: Edge[]) {
+    const newEdges: EdgeType[] = [];
+    for (const edge of source) newEdges.push(...edge.edges());
+    return newEdges;
+  }
+
+  const baseEdges: EdgeType[] = createEdges(baseScenario);
+  const testEdges: EdgeType[] = createEdges(testScenario);
+
+  expect(baseEdges).toStrictEqual(testEdges);
 });
