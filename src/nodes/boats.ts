492ec5013322108e510c8f4e2964d6820cd07416
Kaine Bruce <kaineyb@gmail.com>
2024-04-20 18:00:36 +0100

    boats now correct for one ways

--- src/nodes/boats.ts
+++ src/nodes/boats.ts
@@ -2,43 +2,49 @@ import type { BasicEdge } from "./types";
 import { generateEdgesFromBasic } from "./utils";
 
 const originalBoats: BasicEdge[] = [
-  ["Abah's Landing", "Vivec", "Unnamed Sailboat"], // One way
-  ["Abah's Landing", "Windhelm", "Harbor Skiff"],
   ["Abah's Landing", "Woodhearth", "Harbor Skiff"],
   ["Abah's Landing", "Wayrest", "Harbor Skiff"],
-  // Anvil
-  ["Anvil", "Vivec", "The Razorbill"], // One way
-  ["Anvil", "Vulkhel Guard", "The Egret"],
-  ["Anvil", "Davon's Watch", "The Osprey"],
-  ["Anvil", "Daggerfall", "The Heron"],
+
   ["Anvil", "Shimmerene", "The Golden Gryphon"],
-  // Daggerfall
-  ["Daggerfall", "Anvil", "Harbor Skiff"],
-  ["Daggerfall", "Davon's Watch", "The Hotspur"],
-  ["Daggerfall", "Vulkhel Guard", "The Lydia"],
-  //  Murkmire Boat
+
   ["Daggerfall", "Lilmoth", "Southern Dock - Murkmire Boat"],
-  // Davon's Watch
-  ["Davon's Watch", "Anvil", "Harbor Skiff"],
-  ["Davon's Watch", "Vulkhel Guard", "The Rusty Argonian Blade"],
-  ["Davon's Watch", "Daggerfall", "Summerwind"],
-  // Mournhold
-  ["Mournhold", "Vivec", "Unnamed Sailboat"],
-  // Vivec
-  ["Vivec", "Mournhold", "Coastal Skiff"],
-  // Vulkhel Guard
-  ["Vulkhel Guard", "Anvil", "Harbor Skiff"],
-  ["Vulkhel Guard", "Davon's Watch", "Harbor Skiff"],
-  ["Vulkhel Guard", "Daggerfall", "The Interim Suitor"],
+
   ["Vulkhel Guard", "Lilmoth", "Murkmire Boat"],
-  // Wayrest
-  ["Wayrest", "Vivec", "Unnamed Sailboat"], // One way
+
   ["Wayrest", "Abah's Landing", "Harbor Skiff"],
-  // Windhelm
-  ["Windhelm", "Abah's Landing", "Longboat"],
-  // Woodhearth
-  ["Woodhearth", "Vivec", "Unnamed Sailboat"], // One way
+
   ["Woodhearth", "Abah's Landing", "Harbor Skiff"],
+
+  // One Ways w/ Same Via
+  ["Abah's Landing", "Vivec", "Unnamed Sailboat", true], // One way
+  ["Anvil", "Vivec", "The Razorbill", true], // One way
+  ["Wayrest", "Vivec", "Unnamed Sailboat", true], // One way
+  ["Woodhearth", "Vivec", "Unnamed Sailboat", true], // One way
+
+  // One Ways w/ Different Via:
+  ["Daggerfall", "Anvil", "Harbor Skiff", true], // One way
+  ["Anvil", "Daggerfall", "The Heron", true], // One way
+
+  ["Daggerfall", "Davon's Watch", "The Hotspur", true], // One way
+  ["Davon's Watch", "Daggerfall", "Summerwind", true], // One way
+
+  ["Vivec", "Mournhold", "Coastal Skiff", true], // One way
+  ["Mournhold", "Vivec", "Unnamed Sailboat", true], // One way
+
+  ["Davon's Watch", "Anvil", "Harbor Skiff", true], // One way
+  ["Anvil", "Davon's Watch", "The Osprey", true], // One way
+
+  ["Vulkhel Guard", "Anvil", "Harbor Skiff", true], // One way
+  ["Anvil", "Vulkhel Guard", "The Egret", true], // One way
+
+  ["Davon's Watch", "Vulkhel Guard", "The Rusty Argonian Blade", true], // One way
+  ["Vulkhel Guard", "Davon's Watch", "Harbor Skiff", true], // One way
+
+  ["Daggerfall", "Vulkhel Guard", "The Lydia", true], // One way
+  ["Vulkhel Guard", "Daggerfall", "The Interim Suitor", true], // One way
+
+  ["Windhelm", "Abah's Landing", "Longboat", true], // One way
+  ["Abah's Landing", "Windhelm", "Harbor Skiff", true], // One way
 ];
 
 export const boats = generateEdgesFromBasic(originalBoats, "Boats");
