49a8d5438309d83a0d509c0b81e58121cf221428
Kaine Bruce <kaineyb@gmail.com>
2024-04-20 14:17:27 +0100

    updated combination test

--- src/nodes/tests/combinatons.test.ts
+++ src/nodes/tests/combinatons.test.ts
@@ -1,26 +1,11 @@
-// sum.test.js
 import { expect, test } from "vitest";
 
-import { starts, ends } from "../all";
+import { allCombinations } from "../all";
 
-test("Test Potential Combination Count", () => {
-  const combinations: string[][] = [];
-
-  for (const start of starts) {
-    for (const end of ends) {
-      if (start === end) continue;
-      const combination = [start, end];
-
-      combinations.push(combination);
-    }
-  }
-
-  const same = combinations.filter((location) => location[0] === location[1]);
+test("Test that Combinations does not include self references", () => {
+  const same = allCombinations.filter(
+    (location) => location[0] === location[1]
+  );
 
   expect(same).toStrictEqual([]);
-
-  console.log(combinations);
-  console.log(combinations.length);
-
-  //
 });
