674556c69a0e7030a7f7e014dd122765cbbc9279
Kaine Bruce <kaineyb@gmail.com>
2024-04-20 18:30:48 +0100

    One Way w/ different vias

--- src/nodes/siltstriders.ts
+++ src/nodes/siltstriders.ts
@@ -2,23 +2,32 @@ import type { BasicEdge } from "./types";
 import { generateEdgesFromBasic } from "./utils";
 
 export const siltstridersOG: BasicEdge[] = [
-  ["Balmora", "Gnisis", "Nevos Sareloth"],
-  ["Balmora", "Vivec", "Nevos Sareloth"],
-  ["Gnisis", "Balmora", "Amili Yahaz"],
-  ["Gnisis", "Vivec", "Amili Yahaz"],
-  ["Molag Mar", "Suran", "Narisa Androm"],
-  ["Molag Mar", "Tel Mora", "Narisa Androm"],
   ["Seyda Neen", "Gnisis", "Medyn Hleran"],
-  ["Seyda Neen", "Suran", "Medyn Hleran"],
-  ["Suran", "Molag Mar", "Faven Thendas"],
-  ["Suran", "Seyda Neen", "Faven Thendas"],
-  ["Suran", "Vivec", "Faven Thendas"],
-  ["Tel Mora", "Molag Mar", "Adosi Delvi"],
-  ["Tel Mora", "Vivec", "Adosi Delvi"],
-  ["Vivec", "Balmora", "Helseth Sadalvel"],
-  ["Vivec", "Gnisis", "Helseth Sadalvel"],
-  ["Vivec", "Suran", "Helseth Sadalvel"],
-  ["Vivec", "Tel Mora", "Helseth Sadalvel"],
+
+  // One Way w/ different vias
+  ["Gnisis", "Balmora", "Amili Yahaz", true], // One Way
+  ["Balmora", "Gnisis", "Nevos Sareloth", true], // One Way
+
+  ["Molag Mar", "Suran", "Narisa Androm", true], // One Way
+  ["Suran", "Molag Mar", "Faven Thendas", true], // One Way
+
+  ["Suran", "Seyda Neen", "Faven Thendas", true], // One Way
+  ["Seyda Neen", "Suran", "Medyn Hleran", true], // One Way
+
+  ["Tel Mora", "Molag Mar", "Adosi Delvi", true], // One Way
+  ["Molag Mar", "Tel Mora", "Narisa Androm", true], // One Way
+
+  ["Vivec", "Balmora", "Helseth Sadalvel", true], // One Way
+  ["Balmora", "Vivec", "Nevos Sareloth", true], // One Way
+
+  ["Vivec", "Gnisis", "Helseth Sadalvel", true], // One Way
+  ["Gnisis", "Vivec", "Amili Yahaz", true], // One Way
+
+  ["Suran", "Vivec", "Faven Thendas", true], // One Way,
+  ["Vivec", "Suran", "Helseth Sadalvel", true], // One Way,
+
+  ["Vivec", "Tel Mora", "Helseth Sadalvel", true], // One Way,
+  ["Tel Mora", "Vivec", "Adosi Delvi", true], // One Way
 ];
 
 export const siltstriders = generateEdgesFromBasic(
