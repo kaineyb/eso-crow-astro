a2edb9c27f3f0a584f884533954d539291e7d720
Kaine Bruce <kaineyb@gmail.com>
2024-04-20 18:45:42 +0100

    removed "duplicate" basic edges

--- src/nodes/caravan.ts
+++ src/nodes/caravan.ts
@@ -2,12 +2,9 @@ import type { BasicEdge } from "./types";
 import { generateEdgesFromBasic } from "./utils";
 
 const originalBaandariCaravanStop: BasicEdge[] = [
-  ["Rimmen", "Elden Root", "Zemmarga"],
   ["Elden Root", "Rimmen", "Zemmarga"],
   ["Rimmen", "Wayrest", "Zemmarga"],
-  ["Wayrest", "Rimmen", "Zemmarga"],
   ["Rimmen", "Mournhold", "Zemmarga"],
-  ["Mournhold", "Rimmen", "Zemmarga"],
 ];
 
 export const baandariCaravanStop = generateEdgesFromBasic(
