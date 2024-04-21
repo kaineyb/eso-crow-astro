8ee023973c79e48383631af669d1c70057b90aa5
Kaine Bruce <kaineyb@gmail.com>
2024-04-21 21:35:45 +0100

    added test for primevue

---
+++ src/components/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  <Button
+    class="form-button p-button-sm"
+    label="Button"
+  />
+</template>
+
+<script setup>
+import Button from "primevue/button";
+</script>
+<style scoped></style>
