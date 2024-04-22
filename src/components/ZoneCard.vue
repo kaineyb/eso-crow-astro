514b3a8aac7d91312f2094de80e86785f1c0092b
Kaine Bruce <kaineyb@gmail.com>
2024-04-22 22:08:40 +0100

    started a ZoneCard Component

---
+++ src/components/ZoneCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="outer-container">
+    <div>{{ zoneName }} - {{ zoneType }}</div>
+
+    <ul>
+      <li v-for="city in cities">
+        {{ city }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["zoneName", "zoneType", "cities"]);
+</script>
+<style scoped>
+.outer-container {
+  width: 200px;
+  padding: 10px;
+  border: 1px solid red;
+}
+</style>
