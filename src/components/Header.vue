1eba0d710c3483290713e3e77eb88494a9ac16c7
Kaine Bruce <kaineyb@gmail.com>
2024-04-22 22:18:41 +0100

    added locations page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,7 @@
     <li><a href="/">Home</a></li>
     <li><a href="/routes">Routes</a></li>
     <li><a href="/zones">Zones</a></li>
+    <li><a href="/locations">Locations</a></li>
   </ul>
 </template>
 <script setup></script>
