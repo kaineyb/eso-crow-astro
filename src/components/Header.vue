f98a89523870d37654edb95762a4aa00a53cbe19
Kaine Bruce <kaineyb@gmail.com>
2024-04-22 22:08:27 +0100

    added header and footer components

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/routes">Routes</a></li>
+    <li><a href="/zones">Zones</a></li>
+  </ul>
+</template>
+<script setup></script>
+<style scoped></style>
