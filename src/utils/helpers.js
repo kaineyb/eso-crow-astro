9a41fc87b708165aa74b1e338390651fbf2331b0
Kaine Bruce <kaineyb@gmail.com>
2024-04-24 21:05:57 +0100

    removed func

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14,19 +14,6 @@ export function generateStuff() {
   return output;
 }
 
-export function combinationsToAstro() {
-  const output = [];
-
-  for (const [start, end] of potentialCombinations) {
-    output.push({
-      params: { start: start, end: end },
-      props: { person: "Kaine" },
-    });
-  }
-
-  return output;
-}
-
 export function symmetricDifference(setA, setB) {
   const difference = new Set([...setA].filter((x) => !setB.has(x)));
   for (const elem of setB) {
