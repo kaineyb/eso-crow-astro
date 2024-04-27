ad6ba1df27368aa2a94a563507f5887f4aaf9b6d
Kaine Bruce <kaineyb@gmail.com>
2024-04-27 22:25:06 +0100

    ColdHarbour has issues.

--- src/nodes/zones.ts
+++ src/nodes/zones.ts
@@ -33,7 +33,9 @@ export const dc = [
   "Stros M'Kai",
 ];
 
-export const neutral = ["Coldharbour", "Craglorn", "Cyrodiil"];
+// ColdHarbour has issues.?
+// export const neutral = ["Coldharbour", "Craglorn", "Cyrodiil"];
+export const neutral = ["Craglorn", "Cyrodiil"];
 
 export const chapter = [
   "Artaeum",
