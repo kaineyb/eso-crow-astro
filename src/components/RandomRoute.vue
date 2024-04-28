07f53421719abd5b7562503457b701fdce97429f
Kaine Bruce <kaineyb@gmail.com>
2024-04-28 09:52:11 +0100

    Added Random Route Component

---
+++ src/components/RandomRoute.vue
@@ -0,0 +1,35 @@
+<template>
+  <a
+    @click="generateRoute"
+    :href="url"
+    >Random Route</a
+  >
+</template>
+<script setup>
+import { nodes } from "../nodes/all";
+import { ref, computed } from "vue";
+
+const nodesArray = Array.from(nodes);
+
+const source = ref("?");
+const target = ref("?");
+
+const url = computed(() => `/routes/${source.value}/${target.value}`);
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max + 1);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+function generateRoute() {
+  const sourceIndex = getRandomInt(0, nodesArray.length);
+  let targetIndex = getRandomInt(0, nodesArray.length);
+  while (sourceIndex === targetIndex)
+    targetIndex = getRandomInt(0, nodesArray.length);
+
+  source.value = nodesArray[sourceIndex];
+  target.value = nodesArray[targetIndex];
+}
+</script>
+<style scoped></style>
