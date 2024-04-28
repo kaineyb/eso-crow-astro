981ac171b3498c05ce667ec83575379309cc3fdf
Kaine Bruce <kaineyb@gmail.com>
2024-04-28 12:55:45 +0100

    add float label

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -6,9 +6,12 @@ import "primevue/resources/themes/aura-light-green/theme.css";
 import Button from "primevue/button";
 import Calendar from "primevue/calendar";
 import AutoComplete from "primevue/autocomplete";
+import FloatLabel from "primevue/FloatLabel";
+
 export default (app: App) => {
   app.use(PrimeVue);
   app.component("Button", Button);
   app.component("Calendar", Calendar);
   app.component("AutoComplete", AutoComplete);
+  app.component("FloatLabel", FloatLabel);
 };
