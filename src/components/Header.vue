21230063f443bd3f68bead510ead536c92c30cd1
Kaine Bruce <kaineyb@gmail.com>
2024-04-29 21:43:14 +0100

    added random route to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,12 @@
     <li><a href="/routes">Routes</a></li>
     <li><a href="/zones">Zones</a></li>
     <li><a href="/locations">Locations</a></li>
+    <li>
+      <RandomRoute mode="text" />
+    </li>
   </ul>
 </template>
-<script setup></script>
+<script setup>
+import RandomRoute from "../components/RandomRoute.vue";
+</script>
 <style scoped></style>
