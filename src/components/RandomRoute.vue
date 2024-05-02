807555b1ba51dc214e7aeaf8dabfa044b2a5526d
Kaine Bruce <kaineyb@gmail.com>
2024-05-02 20:53:09 +0100

    change random route

--- src/components/RandomRoute.vue
+++ src/components/RandomRoute.vue
@@ -1,48 +1,26 @@
 <template>
   <a
     v-if="props.mode === 'text'"
-    @click="generateRoute"
+    @click="() => (randomUrl = generateRoute())"
     :href="url"
     >Random Route</a
   >
 </template>
 <script setup>
 import { nodes } from "../nodes/all";
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 
-import { getRandomInt } from "../utils/helpers";
+import { generateRoute } from "../utils/helpers";
 
 const props = defineProps(["mode"]);
 
-const nodesArray = Array.from(nodes);
-
-const defaultValue = "?";
-
-const source = ref(defaultValue);
-const target = ref(defaultValue);
+const randomUrl = ref(null);
 
 const url = computed(() => {
-  if (!source.value || !source.value) return "#";
-  return `/routes/${source.value}/${target.value}`;
+  if (randomUrl.value === null) return "/routes/?/?";
+  return generateRoute();
 });
 
-function generateRoute() {
-  let sourceNode = undefined;
-  let targetNode = undefined;
-
-  while (!sourceNode) {
-    const index = getRandomInt(0, nodesArray.length);
-    sourceNode = nodesArray[index];
-  }
-
-  while (!targetNode) {
-    const index = getRandomInt(0, nodesArray.length);
-    const node = nodesArray[index];
-    if (node != sourceNode) targetNode = node;
-  }
-
-  source.value = sourceNode;
-  target.value = targetNode;
-}
+onMounted(() => (randomUrl.value = null));
 </script>
 <style scoped></style>
